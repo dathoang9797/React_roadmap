1f1e252611a17e79cc02d54d27764eeb5d6bb842
DESKTOP-9C48CIB\datho <dat>
2025-01-20 19:58:34 +0700

    Up

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -7,6 +7,14 @@ import './App.css'
 function App() {
   return (
     <>
+      <a
+        className='"App-link"'
+        href="https://reactjs.org"
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Learn React
+      </a>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import path from 'path';
 import crypto from 'crypto';
 
@@ -23,8 +22,12 @@ export default defineConfig({
       },
     },
   },
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, './src')
+    },
+  },
   plugins: [
     react(),
-    tsconfigPaths()
   ],
 })
