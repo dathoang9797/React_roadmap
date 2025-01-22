46cd13142f2bf2bec9b82fe942d37699575711bd
DESKTOP-9C48CIB\datho <dat>
2025-01-22 21:28:02 +0700

    up

--- react-app/src/components/Greet/index.test.tsx
+++ react-app/src/components/Greet/index.test.tsx
@@ -2,15 +2,19 @@ import { render, screen } from "@testing-library/react"
 import { test } from "vitest";
 import { Greet } from ".";
 
-test("Greet component renders correctly", () => {
-    render(<Greet />);
-    const textElement = screen.getByText(/hello/i);
-    expect(textElement).toBeInTheDocument();
+
+describe("Greet component", () => {
+    test("Greet component renders correctly", () => {
+        render(<Greet />);
+        const textElement = screen.getByText(/hello/i);
+        expect(textElement).toBeInTheDocument();
+    })
 })
 
-
-test("Greet renders with a name", () => {
-    render(<Greet name="Dat"/>);
-    const textElement = screen.getByText(`Hello Dat`);
-    expect(textElement).toBeInTheDocument();
+describe("Nested",( ) => {
+    test("Greet renders with a name", () => {
+        render(<Greet name="Dat" />);
+        const textElement = screen.getByText(`Hello Dat`);
+        expect(textElement).toBeInTheDocument();
+    })
 })
