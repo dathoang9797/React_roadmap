29a2c213b4730bcc19d9ffefe1cd7e4e3354f451
DESKTOP-9C48CIB\datho <dat>
2025-02-09 13:53:52 +0700

    up

--- react-app/src/components/applications/applications.test.tsx
+++ react-app/src/components/applications/applications.test.tsx
@@ -5,8 +5,28 @@ import { Application } from "./applications";
 describe("Application", () => {
     test("render correctly", () => {
         render(<Application />);
-        const nameElement = screen.getByRole("textbox");
+
+        const pageHeading = screen.getByRole("heading", {
+            name: "Job application form",
+            level: 1,
+        });
+        expect(pageHeading).toBeInTheDocument();
+
+        const sectionHeading = screen.getByRole("heading", {
+            name: "Section 1",
+            level: 2,
+        });
+        expect(sectionHeading).toBeInTheDocument();
+
+        const nameElement = screen.getByRole("textbox", {
+            name: "Name"
+        });
         expect(nameElement).toBeInTheDocument();
+
+        const bioElement = screen.getByRole("textbox", {
+            name: "Bio"
+        });
+        expect(bioElement).toBeInTheDocument();
 
         const jobLocationElement = screen.getByRole("combobox");
         expect(jobLocationElement).toBeInTheDocument();
