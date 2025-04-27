4e8255bd462210f1ae9cd2fabb209885a393d016
DESKTOP-9C48CIB\datho <dat>
2025-04-27 20:33:25 +0700

    up

--- react-app/src/components/mui/index.test.tsx
+++ react-app/src/components/mui/index.test.tsx
@@ -2,7 +2,6 @@ import { render, screen } from "../../test-utils";
 import { MuiMode } from "./index";
 import { describe } from "vitest";
 
-// https://www.youtube.com/watch?v=Ru4V8yCR6jQ&list=PLC3y8-rFHvwirqe1KHFCHJ0RqNuN61SJd&index=40
 describe("MuiMode", () => {
     test("render text correctly", () => {
         render(<MuiMode />);
