ec713b71cf30086b34c32d7ea3ec233240f5ed72
DESKTOP-9C48CIB\datho <dat>
2025-04-28 19:31:58 +0700

    up

--- react-app/src/components/users/index.test.tsx
+++ react-app/src/components/users/index.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import { Users } from '.'
+import { http, HttpResponse } from 'msw'
+import { server } from '@src/mocks/server'
 // import { http, HttpResponse } from 'msw'
 // import { server } from '../../mocks/server'
 
@@ -17,20 +19,20 @@ describe('Users', () => {
     expect(users).toHaveLength(3)
   })
 
-  // test('renders error', async () => {
-  //   server.use(
-  //     http.get(
-  //       'https://jsonplaceholder.typicode.com/users',
-  //       () => {
-  //         return HttpResponse.json(
-  //           { message: 'Error fetching users' },
-  //           { status: 500 }
-  //         )
-  //       }
-  //     )
-  //   )
-  //   render(<Users />)
-  //   const error = await screen.findByText('Error fetching users')
-  //   expect(error).toBeInTheDocument()
-  // })
+  test('renders error', async () => {
+    server.use(
+      http.get(
+        'https://jsonplaceholder.typicode.com/users',
+        () => {
+          return HttpResponse.json(
+            { message: 'Error fetching users' },
+            { status: 500 }
+          )
+        }
+      )
+    )
+    render(<Users />)
+    const error = await screen.findByText('Error fetching users')
+    expect(error).toBeInTheDocument()
+  })
 })
